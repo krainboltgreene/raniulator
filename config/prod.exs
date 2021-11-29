f45db06d163ffb3d78f909875f3c9f50d53ae1f0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2021-11-28 20:21:11 -0800

    Fixing syntax error

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ import Config
 # before starting your production server.
 config :raniulator, RaniulatorWeb.Endpoint,
   url: [scheme: "https", host: "infinite-reef-72763.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
